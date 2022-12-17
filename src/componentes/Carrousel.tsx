52891781aeca3c943e1be13475fcd849454479df
Nordín <ghazi_er@hotmail.com>
2022-12-17 14:25:05 +0100

    actualiazr

--- src/componentes/Carrousel.tsx
+++ src/componentes/Carrousel.tsx
@@ -1,15 +1,14 @@
 import React, { useState } from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
-
-const CarouselImg = styled.img `
+const CarouselImg = styled.img`
   display: block;
   width: 100%;
   height: auto;
   margin-left: auto;
   margin-right: auto;
-  border-radius:15px ;
-  overflow : hidden ;
+  border-radius: 15px;
+  overflow: hidden;
   opacity: 0.5;
   transition: 1s;
   &.loaded {
@@ -18,22 +17,19 @@ const CarouselImg = styled.img `
 `;
 
 const CarouselContainer = styled.div`
-display: block;
-align-content : center;
-flex-direction: row;
-margin-top: 15px;
-
-`
-const CarouselButton =styled.button`
-color: white;
-background-color :#1d3434;
-padding :8px;
-margin :5px;
-
-`
+  display: block;
+  align-content: center;
+  flex-direction: row;
+  margin-top: 15px;
+`;
+const CarouselButton = styled.button`
+  color: white;
+  background-color: #1d3434;
+  padding: 8px;
+  margin: 5px;
+`;
 
 export default function Carrousel(props) {
-  
   const images = ["gato1.jpg", "gato2.jpg", "gato3.jpg"];
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [selectedImage, setSelectedImage] = useState(images[0]);
@@ -41,23 +37,20 @@ export default function Carrousel(props) {
 
   const selectNewImage = (index: number, images: string[], next = true) => {
     setLoaded(false);
-    setTimeout(()=>{
+    setTimeout(() => {
       const condition = next
-      ? selectedIndex < images.length - 1
-      : selectedIndex > 0;
-    const nextIndex = next
-      ? condition
-        ? selectedIndex + 1
-        : 0
-      : condition
-      ? selectedIndex - 1
-      : images.length - 1;
-    setSelectedImage(images[nextIndex]);
-    setSelectedIndex(nextIndex);
-
-
-    },500)
-    
+        ? selectedIndex < images.length - 1
+        : selectedIndex > 0;
+      const nextIndex = next
+        ? condition
+          ? selectedIndex + 1
+          : 0
+        : condition
+        ? selectedIndex - 1
+        : images.length - 1;
+      setSelectedImage(images[nextIndex]);
+      setSelectedIndex(nextIndex);
+    }, 500);
   };
 
   const previous = () => {
@@ -69,21 +62,23 @@ export default function Carrousel(props) {
 
   return (
     <>
-      	<div className="container ">
-          <h1 className="text-title"> Carousel hecho con SOLO ReactJs</h1>
-      <CarouselImg
-        style={{ height: "550px", width: "700px" }}
-        src={require(`../assets/${selectedImage}`)}
-        alt="Gato"
-        className={loaded ? "loaded" :""}
-        onLoad={() => setLoaded(true)}
-      />
-      <CarouselContainer>
-      <CarouselButton className="borde" onClick={previous}>
-        {"<"}
-      </CarouselButton>
-      <CarouselButton className="borde" onClick={next}>{">"}</CarouselButton>
-      </CarouselContainer>
+      <div className="container ">
+        <h1 className="text-title"> Carousel hecho con SOLO ReactJs</h1>
+        <CarouselImg
+          style={{ height: "auto", width: "auto" }}
+          src={require(`../assets/${selectedImage}`)}
+          alt="Gato"
+          className={loaded ? "loaded" : ""}
+          onLoad={() => setLoaded(true)}
+        />
+        <CarouselContainer>
+          <CarouselButton className="borde" onClick={previous}>
+            {"<"}
+          </CarouselButton>
+          <CarouselButton className="borde" onClick={next}>
+            {">"}
+          </CarouselButton>
+        </CarouselContainer>
       </div>
     </>
   );
